e692f316ded34f17e8f2db894497fae3b8730590
Unknown <ueder.mds.cardoso@gmail.com>
2020-09-22 04:05:37 -0300

    add files

---
+++ src/main/java/dev/uedercardoso/apivideos/Application.java
@@ -0,0 +1,12 @@
+package dev.uedercardoso.apivideos;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class Application {
+	public void main(String[] args) throws InterruptedException {
+		SpringApplication.run(Application.class, args);
+	}
+	
+}
